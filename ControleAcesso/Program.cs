b7e8f8994a4d1bb201dbf7cfb652170bc7e71cea
joaovmendes19 <109252757+joaovmendes19@users.noreply.github.com>
2023-03-23 22:00:05 -0300

    Add files via upload

---
+++ ControleAcesso/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using View;
+namespace projetosenhas 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        { 
+            Menu.ExibirMenu(); 
+        } 
+    
+    } 
+}
